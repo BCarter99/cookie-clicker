f87b3045368ee1bb8e69a8617556512e09cf7c6f
Brandon Carter <brandon.d.carter99@gmail.com>
2021-02-12 21:56:15 -0500

    Added comments

--- cookie-clicker.py
+++ cookie-clicker.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
+# declares path of the chromedriver, initializes driver, and opens the url of the cookie clicker website
 PATH = "/home/brandon/Documents/chromedriver_linux64/chromedriver"
 driver = webdriver.Chrome(PATH)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
+# allows the website to load before trying to find the elements
 driver.implicitly_wait(10)
 
+# locates and defines the cookie to click on, the number of cookies owned, and the upgrades
 cookie = driver.find_element_by_id("bigCookie")
 cookie_count = driver.find_element_by_id("cookies")
 items = [driver.find_element_by_id('productPrice' + str(i)) for i in range(1, -1, -1)]
@@ -14,10 +17,12 @@ items = [driver.find_element_by_id('productPrice' + str(i)) for i in range(1, -1
 actions = ActionChains(driver)
 actions.double_click(cookie)
 
+# clicks the big cookie (1999 times) while checking for available upgrades
 for i in range(2000):
     actions.perform()
     count = cookie_count.text.split(" ")[0]
     count = int(count.replace(",", ""))
+    # checks to see if any upgrades are available, and clicks if yes
     for item in items:
         value = int(item.text)
         if value <= count:
