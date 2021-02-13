bc3afe70561480d10db7db49983788da8d60b61c
Brandon Carter <brandon.d.carter99@gmail.com>
2021-02-12 23:20:33 -0500

    Added a try and except block in order to be able to terminate the code

--- cookie-clicker.py
+++ cookie-clicker.py
@@ -1,32 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
+
+print('Press Ctrl-C to terminate at any time')
+
+
 # declares path of the chromedriver, initializes driver, and opens the url of the cookie clicker website
 PATH = "/home/brandon/Documents/chromedriver_linux64/chromedriver"
 driver = webdriver.Chrome(PATH)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
+
 # allows the website to load before trying to find the elements
 driver.implicitly_wait(10)
+
 
 # locates and defines the cookie to click on, the number of cookies owned, and the upgrades
 cookie = driver.find_element_by_id("bigCookie")
 cookie_count = driver.find_element_by_id("cookies")
 items = [driver.find_element_by_id('productPrice' + str(i)) for i in range(1, -1, -1)]
 
+
 actions = ActionChains(driver)
 actions.double_click(cookie)
 
-# clicks the big cookie (1999 times) while checking for available upgrades
-for i in range(2000):
-    actions.perform()
-    count = cookie_count.text.split(" ")[0]
-    count = int(count.replace(",", ""))
-    # checks to see if any upgrades are available, and clicks if yes
-    for item in items:
-        value = int(item.text)
-        if value <= count:
-            upgrade_actions = ActionChains(driver)
-            upgrade_actions.move_to_element(item)
-            upgrade_actions.click()
-            upgrade_actions.perform()
+
+# runs the code unless user presses Ctrl-C
+try:
+    # clicks the big cookie while checking for available upgrades
+    loop = True
+    while loop:
+        actions.perform()
+        count = cookie_count.text.split(" ")[0]
+        count = int(count.replace(",", ""))
+        # checks to see if any upgrades are available, and clicks if yes
+        for item in items:
+            value = int(item.text)
+            if value <= count:
+                upgrade_actions = ActionChains(driver)
+                upgrade_actions.move_to_element(item)
+                upgrade_actions.click()
+                upgrade_actions.perform()
+
+except KeyboardInterrupt:
+    loop = False
